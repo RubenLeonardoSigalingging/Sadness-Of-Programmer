345df84be6ed2a727eef7aefd4ecc40b022a9e39
Ruben Leonardo Sigalingging <106874389+RubenLeonardoSigalingging@users.noreply.github.com>
2022-07-02 00:43:43 +0700

    Add files via upload

---
+++ Expression.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+
+<!-- HTML Language Code -->
+<html lang="id-ID">
+<!-- HTML ISO Country Codes -->
+<head>
+	<!-- HTML Favicon -->
+	<link rel="icon" type="image/x-icon" href="https://k.top4top.io/p_2373ix9w41.png">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Sang Pembuat Kode Yang Mengalami Kesedihan">
+	<meta name="keywords" content="Kesedihan, Codemaker, Komputer, Ilmu Komputer, Data Sains, Programmer, Cyber, Sadness, IT, HTML, CSS, JS, Sedih, Galau, Patah Hati, Programmer Galau, Kesedihan Programmer.">
+
+
+	<!-- KODE FONT DARI GOOGLE -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Trispace&display=swap" rel="stylesheet">
+
+
+	<!-- KODE SALJU JS -->
+	<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+	<title>Sadness</title>
+</head>
+<body>
+
+
+	<style type="text/css">
+		body{
+			background-color: black;
+			cursor: crosshair;
+		}
+		font{
+			font-family: 'Trispace', sans-serif;
+			cursor: zoom-out;
+		}
+		img{
+			cursor: n-resize;
+		}
+		audio{
+			cursor: pointer;
+		}
+	</style>
+
+
+	<!-- BUAT TEKS KE TENGAH -->
+	<center>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<img src="https://i.gifer.com/RQ3Z.gif" width="180" height="150"><br><br><br>
+
+
+		<!-- KODE MUSIK -->
+		<audio controls>
+			<source src="https://g.top4top.io/m_2373kstgu1.mp3" type="audio/mpeg">
+		</audio><br><br><br>
+		<font size="5" color="white">Orang yang kamu pikir bodoh dan tidak penting adalah seseorang yang datang dari <font color="red" size="5"> <i><b>Tuhan</font></b></i>, yang mungkin mempelajari <font color="red" size="5"> <i><b>kebahagiaan</font></b></i> dari <font color="red" size="5"> <i><b>kesedihan</font></b></i> dan <font color="red" size="5"> <b><i>pengetahuan</font></i></b> dari <font size="5" color="red"> <b><i>kegelapan.</font></i></b></font><br><br><br>
+	</center>
+
+</body>
+</html>
